bde5d977605c9f19daa39cbfd49de30a21eaca2d
Gabriel Eduardo Acosta Nuñez <gacosta.com.do@outlook.com>
2019-08-04 18:02:02 -0400

    Add products controller

---
+++ Shop/Shop.Web/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Web.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Web.Data
+{
+    public class DataContext: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+
+        }
+    }
+}
---
+++ Shop/Shop.Web/Data/Entities/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Web.Data.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [DisplayFormat(DataFormatString ="{0:C2}",ApplyFormatInEditMode =false)]
+        public decimal Price { get; set; }
+        [Display(Name="Image")]
+        public string ImageUrl { get; set; }
+        [Display(Name="Last Purchase")]
+        public DateTime LastPurchase { get; set; }
+        [Display(Name = "Last Sale")]
+        public DateTime LastSale { get; set; }
+        [Display(Name ="Is Avalabe?")]
+        public bool IsAvailable { get; set; }
+        [DisplayFormat(DataFormatString ="{0:N2}",ApplyFormatInEditMode =false)]
+        public double Stock { get; set; }
+
+    }
+}
